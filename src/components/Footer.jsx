dfbef7cd5c4650d8a46925cfe026f799f1444df3
Daniel Sadiq <danielsadiq93@gmail.com>
2026-04-03 22:30:59 +0100

    base components add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div className='bg-black px-5 lg:px-28 py-3 lg:py-6 flex items-center justify-between mt-16'>
+      <a href='#intro' className='text-4xl font-bold text-white'>sadiq.</a>
+      <div className='text-white lg:font-semibold lg:text-sm font-normal text-[10px] text-right lg:space-y-3'>
+        <p>@ 2026 Personal Portfolio</p>
+        <p>Made by -sadiq</p>
+      </div>
+    </div>
+  )
+}
